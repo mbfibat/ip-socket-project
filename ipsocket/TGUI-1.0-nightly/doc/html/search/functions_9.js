c8df3f115ec3d74ce9c8a851c64bca488fd9da9b
sliceoflif3 <yunomiclover@gmail.com>
2023-04-02 23:20:25 +0700

    delete vs proj folder

--- ipsocket/TGUI-1.0-nightly/doc/html/search/functions_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['join_0',['join',['../classtgui_1_1_string.html#aab5c1005976a0026c6edcff4ca756944',1,'tgui::String']]]
-];
--- ipsocket/TGUI-1.0-nightly/tests/cmake/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <TGUI/DefaultBackendWindow.hpp>
-
-int main()
-{
-    auto window = tgui::DefaultBackendWindow::create(250, 100, "TGUI");
-}
